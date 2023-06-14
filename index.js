df9ec880542a013b44fb3d8af616bd593a7f5393
Mark Prettyman <markiesmyname@Marks-iMac.lan>
2023-06-13 21:51:36 -0700

    static methods/algorithms)

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9\-\'\s]/g, '')
+  }
+  static titleize(str) {
+    const noCapitalize = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from', 'the']
+    const words = str.split(' ')
+
+    const titleizeWords = words.map((word, index) => {
+      if (index === 0 || !noCapitalize.includes(word)) {
+        return this.capitalize(word)
+      } else {
+        return word 
+      }
+    })
+return titleizeWords.join(' ')
+  }
 }
